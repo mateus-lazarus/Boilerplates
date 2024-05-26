d46eee90c8714aed400a148fcb182f5b1d052dc0
Mateus_Lazarus (Alexei) <devMateusLazarus@gmail.com>
2024-05-25 21:32:20 -0300

    refact(Tests): Removed duplicated code in tests with common methods.

--- ExpressJSApiBoilerplate/Initial-Test/tests/functionalTests/controllers/homeController.test.js
+++ ExpressJSApiBoilerplate/Initial-Test/tests/functionalTests/controllers/homeController.test.js
@@ -4,11 +4,15 @@ import { expect, describe, test } from '@jest/globals'
 
 const url = process.env.FUNCTIONAL_TESTS_URL;
 
+const getHomeRequest = async (path) => {
+  return await request(url).get(path);
+};
+
 describe('homeController', () => {
   test('GET / should return "Hello, world!"', async () => {
     const expectResult = 'Hello, world!';
 
-    const response = await request(url).get('/');
+    const response = await getHomeRequest('/');
 
     expect(response.statusCode).toBe(200);
     expect(response.body).toBe(expectResult);
@@ -17,7 +21,7 @@ describe('homeController', () => {
   test('GET /about should return "About Page"', async () => {
     const expectResult = 'About Page';
 
-    const response = await request(url).get('/about');
+    const response = await getHomeRequest('/about');
 
     expect(response.statusCode).toBe(200);
     expect(response.body).toBe(expectResult);
--- ExpressJSApiBoilerplate/Initial-Test/tests/functionalTests/controllers/mathController.test.js
+++ ExpressJSApiBoilerplate/Initial-Test/tests/functionalTests/controllers/mathController.test.js
@@ -3,100 +3,104 @@ import request from 'supertest';
 import { expect, describe, test } from '@jest/globals'
 
 const url = process.env.FUNCTIONAL_TESTS_URL;
+const squarePath = '/math/square';
+const doublePath = '/math/double';
+
+const postMathRequest = async (path, input) => {
+  return await request(url)
+    .post(path)
+    .send({ number: input });
+};
+
+const getMathRequest = async (path, input) => {
+  return await request(url).get(`${path}/${input}`);
+};
 
 describe('mathController', () => {
-  test('POST /square should return squared number', async () => {
+  test(`POST ${squarePath} should return squared number`, async () => {
     const inputValue = 5;
     const expectedStatusCode = 200;
     const expectedResult = 25;
 
-    const response = await request(url)
-      .post('/square')
-      .send({ number: inputValue });
+    const response = await postMathRequest(squarePath, inputValue);
     
     expect(response.statusCode).toBe(expectedStatusCode);
     expect(response.body.result).toBe(expectedResult);
   });
 
-  test('POST /square should handle invalid number', async () => {
+  test(`POST ${squarePath} should handle invalid number`, async () => {
     const inputValue = 'abc';
     const expectedStatusCode = 500;
     const expectedResult = 'Invalid number';
 
-    const response = await request(url)
-      .post('/square')
-      .send({ number: inputValue });
+    const response = await postMathRequest(squarePath, inputValue);
 
     expect(response.statusCode).toBe(expectedStatusCode);
     expect(response.body.error.message).toBe(expectedResult);
   });
 
-  test('GET /square should return squared number', async () => {
+  test(`GET ${squarePath} should return squared number`, async () => {
     const inputValue = 5;
     const expectedStatusCode = 200;
     const expectedResult = 25;
 
-    const response = await request(url).get(`/square/${inputValue}`);
+    const response = await getMathRequest(squarePath, inputValue);
     
     expect(response.statusCode).toBe(expectedStatusCode);
     expect(response.body.result).toBe(expectedResult);
   });
   
-  test('GET /square should handle invalid number', async () => {
+  test(`GET ${squarePath} should handle invalid number`, async () => {
     const inputValue = 'abc';
     const expectedStatusCode = 500;
     const expectedResult = 'Invalid number';
 
-    const response = await request(url).get(`/square/${inputValue}`);
+    const response = await getMathRequest(squarePath, inputValue);
 
     expect(response.statusCode).toBe(expectedStatusCode);
     expect(response.body.error.message).toBe(expectedResult);
   });
 
-  test('POST /double should return doubled number', async () => {
+  test(`POST ${doublePath} should return doubled number`, async () => {
     const inputValue = 5;
     const expectedStatusCode = 200;
     const expectedResult = 10;
 
-    const response = await request(url)
-      .post('/double')
-      .send({ number: inputValue });
+    const response = await postMathRequest(doublePath, inputValue);
     
-      expect(response.statusCode).toBe(expectedStatusCode);
-      expect(response.body.result).toBe(expectedResult);
+    expect(response.statusCode).toBe(expectedStatusCode);
+    expect(response.body.result).toBe(expectedResult);
   });
 
-  test('POST /double should handle invalid number', async () => {
+  test(`POST ${doublePath} should handle invalid number`, async () => {
     const inputValue = 'abc';
     const expectedStatusCode = 500;
     const expectedResult = 'Invalid number';
 
-    const response = await request(url)
-      .post('/double')
-      .send({ number: inputValue });
+    const response = await postMathRequest(doublePath, inputValue);
 
     expect(response.statusCode).toBe(expectedStatusCode);
     expect(response.body.error.message).toBe(expectedResult);
   });
 
-  test('GET /double should return doubled number', async () => {
+  test(`GET ${doublePath} should return doubled number`, async () => {
     const inputValue = 5;
     const expectedStatusCode = 200;
     const expectedResult = 10;
 
-    const response = await request(url).get(`/double/${inputValue}`);
+    const response = await getMathRequest(doublePath, inputValue);
     
     expect(response.statusCode).toBe(expectedStatusCode);
     expect(response.body.result).toBe(expectedResult);
   });
 
 
-  test('GET /double should handle invalid number', async () => {
+  test(`GET ${doublePath} should handle invalid number`, async () => {
     const inputValue = 'abc';
     const expectedStatusCode = 500;
     const expectedResult = 'Invalid number';
 
-    const response = await request(url).get(`/double/${inputValue}`);
+    const response = await getMathRequest(doublePath, inputValue);
 
     expect(response.statusCode).toBe(expectedStatusCode);
     expect(response.body.error.message).toBe(expectedResult);
